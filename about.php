590783dac0e5b3c55d90b64fe6e6d121b1fd70ba
patrick-cw <patrick1528@gmail.com>
2021-05-24 20:58:09 +0700

    init

---
+++ about.php
@@ -0,0 +1,5 @@
+<h1>About</h1>
+<p>
+    Nama: Patrick Cipta Winata<br>
+    NRP: 05111840000098<br>
+</p>
---
+++ contact.php
@@ -0,0 +1,5 @@
+<h1>Contact</h1>
+<p>
+    Nomor Telepon: +62811<br>
+    Email: Contact@gmail.com<br>
+</p>
